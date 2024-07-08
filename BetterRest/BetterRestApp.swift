f08fbc8f869a0bdd813d3390f2736e199f4d358b
HigasaOR <lee1003094395@gmail.com>
2024-07-08 22:17:00 +0800

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Chien Lee on 2024/7/8.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
